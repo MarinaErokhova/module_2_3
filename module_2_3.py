517e0c804df3b4ef5ee162e2967e53fc4eed6d39
MarinaErokhova <erokhovamn@gmail.com>
2024-09-29 21:04:14 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,14 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5] #задаем список
+print(my_list) #вывод списка в консоль
+i = 0 #начальное значение индекса
+while i < len(my_list):
+    number = my_list [i] #число из списка
+    i += 1 #увеличить значение переменной на 1
+    if number == 0:
+        continue
+    elif number < 0:
+        break
+    elif i == len(my_list):
+        print(number)
+    else:
+        print(number)
